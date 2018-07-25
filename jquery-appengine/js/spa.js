eba7c584cf662a827568b08c5a141a46c880a721
Eric Schmidt <erschmid@google.com>
2018-07-25 10:19:18 -0700

    Fixing bug in spa sample.

--- jquery-appengine/js/spa.js
+++ jquery-appengine/js/spa.js
@@ -2,7 +2,7 @@ window.onload = function () {
     $(".display div").hide();
     $(".display div").first().show();
 
-    $(".page-1").click(function () {
+    $("button.page-1").click(function () {
            $(".display div").hide();
            $(".display .page-2").first().show();
     });
